57832a62eeb86dec3446adbe3b173b7074159a97
Gorr <gorrgorrob@gmail.com>
2021-03-28 20:14:15 +0500

    Code refactoring

--- src/main/kotlin/net/catstack/editor/ui/level/settings/LevelSettings.kt
+++ src/main/kotlin/net/catstack/editor/ui/level/settings/LevelSettings.kt
@@ -1,21 +1,15 @@
 package net.catstack.editor.ui.level.settings
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.pointer.pointerMoveFilter
 import androidx.compose.ui.unit.dp
 import net.catstack.editor.Window
 import net.catstack.editor.ui.level.LevelViewModel
@@ -23,67 +17,54 @@ import net.catstack.editor.ui.level.LevelViewModel
 @Composable
 private fun LevelSettingsView(viewModel: LevelViewModel) {
     Column(modifier = Modifier.fillMaxSize()) {
-        val levelModel = remember { viewModel.levelModel }.value ?: return
-        val levelName by remember { viewModel.levelName }
-        val selectedWave by remember { viewModel.selectedWave }
-        val saved by remember { viewModel.saved }
-        val savedText = if (saved) " - saved!" else ""
-
         Window.window.keyboard.setShortcut(Key.Escape, viewModel::unselectWave)
 
-        Text(
-            levelName + savedText,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            style = MaterialTheme.typography.h6
+        LevelName(viewModel, Modifier.align(Alignment.CenterHorizontally))
+
+        WavesList(
+            viewModel,
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(top = 16.dp)
         )
-        Text("Waves:")
-
-        Column(modifier = Modifier
-            .fillMaxWidth()
-            .weight(1f)
-            .padding(top = 16.dp)
-        ) {
-            val selectionColor = MaterialTheme.colors.primary
-            val selectionBgColor = Color(0x22FFFFFF)
-            levelModel.wavesData.forEachIndexed { index, waveModel ->
-                val color = remember { mutableStateOf(Color.Unspecified) }
-                val bgColor = remember { mutableStateOf(Color.Unspecified) }
-                Column(modifier = Modifier
-                    .background(color = if (waveModel == selectedWave) selectionBgColor else Color.Unspecified)
-                ) {
-                    Divider(modifier = Modifier.fillMaxWidth())
-
-                    Text("Wave ${index + 1}", modifier = Modifier
-                        .pointerMoveFilter(onEnter = {
-                            color.value = selectionColor
-                            return@pointerMoveFilter true
-                        }, onExit = {
-                            color.value = Color.Unspecified
-                            return@pointerMoveFilter true
-                        })
-                        .fillMaxWidth()
-                        .clickable {
-                            bgColor.value = selectionBgColor
-                            viewModel.selectWave(waveModel)
-                        }
-                        .padding(8.dp),
-                        color = if (waveModel == selectedWave) selectionColor else color.value)
-                }
-            }
-            if (levelModel.wavesData.isNotEmpty())
-                Divider(modifier = Modifier.fillMaxWidth())
-        }
 
         Column(modifier = Modifier.fillMaxWidth().height(60.dp), verticalArrangement = Arrangement.Center) {
-            Button(
-                onClick = {
-                    viewModel.addWave()
-                },
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            ) {
-                Text("New wave")
-            }
+            NewWaveButton(viewModel, modifier = Modifier.align(Alignment.CenterHorizontally))
         }
+    }
+}
+
+@Composable
+private fun LevelName(viewModel: LevelViewModel, modifier: Modifier = Modifier) {
+    val levelName by remember { viewModel.levelName }
+    val saved by remember { viewModel.saved }
+    val savedText = if (saved) " - saved!" else ""
+
+    Text(
+        levelName + savedText,
+        modifier = modifier,
+        style = MaterialTheme.typography.h6
+    )
+}
+
+@Composable
+private fun WavesList(viewModel: LevelViewModel, modifier: Modifier = Modifier) {
+    val levelModel = remember { viewModel.levelModel }.value ?: return
+
+    Text("Waves:")
+    Column(modifier = modifier) {
+        WavesList(levelModel, viewModel)
+    }
+}
+
+@Composable
+private fun NewWaveButton(viewModel: LevelViewModel, modifier: Modifier = Modifier) {
+    Button(
+        onClick = viewModel::addWave,
+        modifier = modifier
+    ) {
+        Text("New wave")
     }
 }
 
---
+++ src/main/kotlin/net/catstack/editor/ui/level/settings/WaveListView.kt
@@ -0,0 +1,59 @@
+package net.catstack.editor.ui.level.settings
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerMoveFilter
+import androidx.compose.ui.unit.dp
+import net.catstack.editor.models.LevelModel
+import net.catstack.editor.models.WaveModel
+import net.catstack.editor.ui.level.LevelViewModel
+
+@Composable
+fun WavesList(levelModel: LevelModel, viewModel: LevelViewModel) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Divider(modifier = Modifier.fillMaxWidth())
+        levelModel.wavesData.forEachIndexed { index, waveModel ->
+            WavesListItem(waveModel, viewModel, index)
+        }
+    }
+}
+
+@Composable
+fun WavesListItem(waveModel: WaveModel, viewModel: LevelViewModel, waveIndex: Int) {
+    val selectedWave = remember { viewModel.selectedWave }.value
+
+    val selectionColor = MaterialTheme.colors.primary
+    val selectionBgColor = Color(0x22FFFFFF)
+
+    val textColor = remember { mutableStateOf(Color.Unspecified) }
+    val bgColor = if (waveModel == selectedWave) selectionBgColor else Color.Unspecified
+
+    Column(modifier = Modifier.background(color = bgColor)) {
+        Text("Wave ${waveIndex + 1}", modifier = Modifier
+            .pointerMoveFilter(onEnter = {
+                textColor.value = selectionColor
+                return@pointerMoveFilter true
+            }, onExit = {
+                textColor.value = Color.Unspecified
+                return@pointerMoveFilter true
+            })
+            .fillMaxWidth()
+            .clickable { viewModel.selectWave(waveModel) }
+            .padding(8.dp),
+            color = if (waveModel == selectedWave) selectionColor else textColor.value)
+
+        Divider(modifier = Modifier.fillMaxWidth())
+    }
+}
